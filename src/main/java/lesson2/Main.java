689876b97b067339ce30f4cdc8deea35827c34a5
Vladimir Nastoyaschiy <maga_net@mail.ru>
2018-08-12 20:27:02 +0300

    создан метод обработки команды

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -24,11 +24,10 @@ public class Main {
         String comand;
 
 
-
         Scanner scanner = new Scanner(System.in);
 
         comand = scanner.nextLine();
-        while(!comand.equals("exit")){
+        while (!comand.equals("exit")) {
             if (comand.startsWith("/")) {
                 System.out.println("это команда, сейчас я ее проверю");
                 processComand(comand);
@@ -41,40 +40,58 @@ public class Main {
 
     }
 
-    private static void processComand(String comand) {
-        String[] tokens;
-        String firstToken;
-        
+    private static void processComand(String comand) throws SQLException {
+        String[] tokens = comand.split(" ");
+        String firstToken = tokens[0];
+
+        if (comand.equals("/создатьтаблицу")) {
+            createTableGoods();
+        } else if (comand.equals("/удалитьтаблицу")) {
+            dropTableGoods();
+        } else if (comand.equals("/заполнитьтаблицу")) {
+            fillTableGoods();
+        } else if (comand.equals("/очиститьтаблицу")) {
+            clearTable();
+        } else if (firstToken.equals("/цена") && tokens.length == 2) {
+            System.out.println(findCostByTitle(tokens[1]));
+        } else if (firstToken.equals("/сменитьцену") && tokens.length == 3) {
+            System.out.println(changeCostByTitle(tokens[1], Integer.parseInt(tokens[2])));
+        } else if (firstToken.equals("/товарыпоцене") && tokens.length == 3) {
+            showTitlesFromCostRange(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+        } else System.out.println("команда неправильная, попробуй ещё");
+
     }
 
     private static void showTitlesFromCostRange(int fromCost, int toCost) {
 
-            try {
-                connect();
-                ResultSet rs = stmt.executeQuery("SELECT title FROM goods WHERE cost BETWEEN " + fromCost + " AND " + toCost);
-                while(rs.next()){
-                    System.out.println(rs.getString(1));
-                }
-
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    disconnect();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+        try {
+            connect();
+            ResultSet rs = stmt.executeQuery("SELECT title FROM goods WHERE cost BETWEEN " +
+                    fromCost + " AND " + toCost);
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
             }
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                disconnect();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
+
+    }
 
 
     private static boolean changeCostByTitle(String title, int cost) {
         try {
             connect();
-            if (stmt.executeUpdate("UPDATE goods SET cost = " + cost + " WHERE title = \"" + title + "\"") > 0) {
+            if (stmt.executeUpdate("UPDATE goods SET cost = " + cost +
+                    " WHERE title = \"" + title + "\"") > 0) {
                 return true;
             }
 
@@ -113,12 +130,11 @@ public class Main {
     }
 
     private static void fillTableGoods() throws SQLException {
-
-
         try {
             connect();
             stmt.executeUpdate("DELETE FROM goods");
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO goods (id, prodid, title, cost) VALUES (?, ?, ?, ?)");
+            PreparedStatement ps =
+                    connection.prepareStatement("INSERT INTO goods (id, prodid, title, cost) VALUES (?, ?, ?, ?)");
             connection.setAutoCommit(false);
             for (int i = 1; i <= 10000; i++) {
                 ps.setInt(1, i);
@@ -171,6 +187,23 @@ public class Main {
                     "    cost   INTEGER\n" +
                     ");\n");
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                disconnect();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void clearTable() throws SQLException {
+        try {
+            connect();
+            stmt.executeUpdate("DELETE FROM goods");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
