b2c7e07ce6e45e368e0e8bce34298ba30e010943
Vladimir Nastoyaschiy <maga_net@mail.ru>
2018-08-12 16:53:40 +0300

    создал методы подключения/отключения БД

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -1,4 +1,25 @@
 package lesson2;
 
+
+import java.sql.*;
+
 public class Main {
+
+    private static Connection connection;
+    private static Statement stmt;
+    private static ResultSet rs;
+
+
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:lesson2_DB.db");
+        stmt = connection.createStatement();
+    }
+
+    public static void disconnect() throws SQLException {
+        connection.close();
+    }
+
+
 }
+
