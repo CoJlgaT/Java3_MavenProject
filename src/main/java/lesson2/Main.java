b265ceb338fc7ebec65f740086ff340a2fb2deba
Vladimir Nastoyaschiy <maga_net@mail.ru>
2018-08-12 18:57:36 +0300

    методы поиска/смены цены/вывода
    Добавлены методы:
    -поиск цены товара по имени;
    -смена цены товара по имени;
    -вывод имен товаров, у которых цена попадает в заданный диапазон

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -13,14 +13,82 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        createTableGoods();
-        fillTableGoods();
+//        createTableGoods();
+//        fillTableGoods();
+//        System.out.println(findCostByTitle("товар666"));
+//        System.out.println(changeCostByTitle("товар1", 100));
+//        showTitlesFromCostRange(100, 600);
 //        dropTableGoods();
 
 
-        Scanner scanner = new Scanner(System.in);
 
 
+    }
+
+    private static void showTitlesFromCostRange(int fromCost, int toCost) {
+
+            try {
+                connect();
+                ResultSet rs = stmt.executeQuery("SELECT title FROM goods WHERE cost BETWEEN " + fromCost + " AND " + toCost);
+                while(rs.next()){
+                    System.out.println(rs.getString(1));
+                }
+
+
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    disconnect();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+
+    private static boolean changeCostByTitle(String title, int cost) {
+        try {
+            connect();
+            if (stmt.executeUpdate("UPDATE goods SET cost = " + cost + " WHERE title = \"" + title + "\"") > 0) {
+                return true;
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                disconnect();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    private static String findCostByTitle(String title) {
+        try {
+            connect();
+            ResultSet rs = stmt.executeQuery("SELECT cost FROM goods WHERE title = \"" + title + "\"");
+            return rs.getString(1);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                disconnect();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return "Такого товара нет";
     }
 
     private static void fillTableGoods() throws SQLException {
@@ -31,7 +99,7 @@ public class Main {
             stmt.executeUpdate("DELETE FROM goods");
             PreparedStatement ps = connection.prepareStatement("INSERT INTO goods (id, prodid, title, cost) VALUES (?, ?, ?, ?)");
             connection.setAutoCommit(false);
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 1; i <= 10000; i++) {
                 ps.setInt(1, i);
                 ps.setInt(2, i);
                 ps.setString(3, "товар" + i);
