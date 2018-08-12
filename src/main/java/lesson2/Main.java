e307018db9d7894bc09a9ce1b787597c6f508a78
Vladimir Nastoyaschiy <maga_net@mail.ru>
2018-08-12 17:29:16 +0300

    созданы методы создания и удаления таблицы товаров

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -10,6 +10,54 @@ public class Main {
     private static ResultSet rs;
 
 
+    public static void main(String[] args) {
+
+//        createTableGoods();
+        dropTableGoods();
+
+    }
+
+    public static void dropTableGoods(){
+        try {
+            connect();
+            stmt.executeUpdate("DROP TABLE goods");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                disconnect();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void createTableGoods(){
+        try {
+            connect();
+            stmt.executeUpdate("CREATE TABLE goods (\n" +
+                    "    id     INTEGER     PRIMARY KEY,\n" +
+                    "    prodid INTEGER     UNIQUE,\n" +
+                    "    title  STRING (50),\n" +
+                    "    cost   INTEGER\n" +
+                    ");\n");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                disconnect();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void connect() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         connection = DriverManager.getConnection("jdbc:sqlite:lesson2_DB.db");
