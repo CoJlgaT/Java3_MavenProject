6dace77fb22c1e1066d93f0757122036109f5a0e
Vladimir Nastoyaschiy <maga_net@mail.ru>
2018-08-24 23:44:14 +0300

    Сделан одновременный старт
    Использовал CyclicBarrier
    Присутствует закомментированная неудачная попытка использования семаора в тоннеле.

--- src/main/java/lesson5/Car.java
+++ src/main/java/lesson5/Car.java
@@ -1,7 +1,13 @@
 package lesson5;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    Semaphore smp = new Semaphore(CARS_COUNT/2);
+CyclicBarrier cb;
+
     static {
         CARS_COUNT = 0;
     }
@@ -14,11 +20,12 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier cb) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cb = cb;
     }
     @Override
     public void run() {
@@ -26,10 +33,16 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
+            if (race.getStages().get(i) instanceof Tunnel){
+                Tunnel temp_tunnel = (Tunnel)race.getStages().get(i);
+                System.out.println("tonnel");
+                temp_tunnel.go(this,smp);
+            } else
             race.getStages().get(i).go(this);
         }
     }
--- src/main/java/lesson5/Tunnel.java
+++ src/main/java/lesson5/Tunnel.java
@@ -1,5 +1,7 @@
 package lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
     public Tunnel() {
         this.length = 80;
@@ -16,6 +18,24 @@ public class Tunnel extends Stage {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void go(Car c, Semaphore smp) {
+
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+               // smp.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+               // smp.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
