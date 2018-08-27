808c12c202ffe9611b5e2b7a0d2fe04ef3905f8a
Vladimir Nastoyaschiy <maga_net@mail.ru>
2018-08-27 21:32:23 +0300

    написан метод task1

--- src/main/java/lesson6/MainClass.java
+++ src/main/java/lesson6/MainClass.java
@@ -1,4 +1,32 @@
 package lesson6;
 
-public class MainClass {
+import java.util.Arrays;
+
+public class MainClass  {
+    public static int[] task1(int... input) throws RuntimeException {
+
+        int start = input.length;
+        for (int i = input.length-1; i >=0 ; i--) {
+            if (input[i]==4) {
+                start=i;
+                break;
+            }
+        }
+        if (start==input.length) {
+            throw new RuntimeException("Нет четверок во входном массиве");
+        }
+        int[] output= new int[input.length-start-1];
+        for (int i = start+1; i < input.length; i++) {
+            output[i-start-1] = input[i];
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args)  {
+        System.out.println(Arrays.toString(task1(1,2,3,4,5,6,7)));
+        System.out.println(Arrays.toString(task1(1,4,3,4,5,4,7)));
+        System.out.println(Arrays.toString(task1(1,2,3,4,5,6,4)));
+        System.out.println(Arrays.toString(task1(1,2,3,7,5,6,0)));
+    }
 }
